b657f01da7d9ad77c7d73a87e9c2cdc129a48a6d
Adrián Javorček <59730272+adrian-13@users.noreply.github.com>
2024-06-16 11:42:01 +0200

    Update example.js
    feat: Update Merkle Tree proof generation and verification logic
    
    - Added keccak256 hashing to generate leaf nodes for the Merkle Tree.
    - Updated Merkle Tree creation to use hashed leaves.
    - Implemented verification logic for hashed leaf nodes.
    - Exported Merkle root and proof for external use.

--- utils/example.js
+++ utils/example.js
@@ -1,19 +1,27 @@
-const MerkleTree = require('./MerkleTree');
-const niceList = require('./niceList');
-const verifyProof = require('./verifyProof');
+const MerkleTree = require("../utils/MerkleTree");
+const niceList = require("../utils/niceList.json");
+const verifyProof = require("../utils/verifyProof");
+const { keccak256 } = require("ethereum-cryptography/keccak");
+const { bytesToHex } = require("ethereum-cryptography/utils");
 
 // create the merkle tree for the whole nice list
-const merkleTree = new MerkleTree(niceList);
+const leaves = niceList.map((name) => keccak256(Buffer.from(name)));
+const merkleTree = new MerkleTree(leaves);
 
 // get the root
 const root = merkleTree.getRoot();
 
-// find the proof that norman block is in the list 
-const name = 'Norman Block';
-const index = niceList.findIndex(n => n === name);
+console.log(`Merkle Root: ${root}`);
+
+// find the proof that a specific name is in the list
+const name = "Sidney Kertzmann";
+const index = niceList.findIndex((n) => n === name);
 const proof = merkleTree.getProof(index);
 
 // verify proof against the Merkle Root
-console.log( verifyProof(proof, name, root) ); // true, Norman Block is in the list!
+const leaf = keccak256(Buffer.from(name));
+console.log(verifyProof(proof, leaf, root)); // true, Norman Block is in the list!
 
 // TRY IT OUT: what happens if you try a name not in the list, or a fake proof?
+
+module.exports = { root, proof };
